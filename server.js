97d28d22c81f2350ed96e79005d423b51f5b576f
BuildTools <unconfigured@null.spigotmc.org>
2025-04-06 11:00:44 +0700

    update base code

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 app.use(express.json());
-
 const PORT = 3000;
+
+
+
 
 app.listen(PORT, () => {
     console.log("listening " + PORT);
